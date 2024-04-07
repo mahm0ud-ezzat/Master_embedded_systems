240848433ffc6a3784e7252dafcf72f6ff606a5d
mahmoud <mahmoud99@gmail.com>
2024-04-07 14:34:11 +0200

    first assignment

---
+++ unit2/c_basics/EX1_C_Program_To_Print_Sentence.c
@@ -0,0 +1,14 @@
+/*
+ ======================================================================================================================
+ Name        : EX1_C_Program_To_Print_Sentence.c
+ Author      : Mahmoud Ezzat
+ Description : Unit 2 C-Progtamming >> Assignment 1 C-Basic >> EX1 C Program To Print Sentence using Ansi-style (c99)
+ ======================================================================================================================
+ */
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	printf("c programming ");
+	return 0 ;
+}
